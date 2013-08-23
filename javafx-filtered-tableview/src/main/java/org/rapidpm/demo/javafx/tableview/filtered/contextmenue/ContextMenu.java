bdbf7239f4ba7ce66493921c6be2bd3f973be5f5
Sven Ruppert <sven.ruppert@siteos.de>
2013-08-23 21:44:01 +0200

    default context-menue

---
+++ javafx-filtered-tableview/src/main/java/org/rapidpm/demo/javafx/tableview/filtered/contextmenue/ContextMenu.java
@@ -0,0 +1,285 @@
+package org.rapidpm.demo.javafx.tableview.filtered.contextmenue;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.stage.FileChooser;
+import org.rapidpm.demo.cdi.commons.logger.Logger;
+import org.rapidpm.demo.javafx.tableview.filtered.FilteredTableView;
+
+/**
+ * User: Sven Ruppert Date: 14.08.13 Time: 16:30
+ */
+public class ContextMenu<T> extends javafx.scene.control.ContextMenu {
+
+    private static final Logger logger = Logger.getLogger(ContextMenu.class);
+//    private @Inject @CDILogger Logger logger;
+
+    public ContextMenu() {
+
+    }
+
+    private FilteredTableView<T> filteredTableView;
+
+//    @PostConstruct
+    public void init(){
+        MenuItem openCSV = new MenuItem("contextmenu.opencsv");
+        openCSV.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                Platform.runLater(new Runnable() {
+
+                    @Override public void run() {
+                        try {
+                            final StringBuilder stringBuilder = convertTable2CSV();
+                            final File temp = File.createTempFile("xls-temp-" + System.nanoTime(), ".csv");
+                            final FileOutputStream fos = new FileOutputStream(temp);
+                            fos.write(stringBuilder.toString().getBytes());
+                            fos.flush();
+                            fos.close();
+                            Desktop desktop = Desktop.getDesktop();
+                            desktop.open(temp);
+                        } catch (IOException e1) {
+                            logger.error(e1);
+                        }
+                    }
+                });
+            }
+        });
+        MenuItem saveCSV = new MenuItem("contextmenu.savecsv");
+        saveCSV.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                Platform.runLater(new Runnable() {
+                    @Override public void run() {
+                        try {
+                            final StringBuilder stringBuilder = convertTable2CSV();
+                            FileChooser fileChooser = new FileChooser();
+                            fileChooser.setTitle("contextmenu.savecsv");
+                            fileChooser.setInitialDirectory(
+                                    new File(System.getProperty("contextmenu.defaultdir"))
+                            );
+                            final File targetFile = fileChooser.showSaveDialog(filteredTableView.getScene().getWindow());
+                            if (targetFile != null) {
+                                final FileOutputStream fos = new FileOutputStream(targetFile);
+                                fos.write(stringBuilder.toString().getBytes());
+                                fos.flush();
+                                fos.close();
+                            } else {
+                                if (logger.isDebugEnabled()) {
+                                    logger.debug("keine Ziel ausgewählt..");
+                                }
+                            }
+                        } catch (IOException e1) {
+                            logger.error(e1);
+                        }
+                    }
+                });
+            }
+        });
+        final MenuItem openXlS = new MenuItem("contextmenu.openxls");
+        openXlS.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                if (logger.isDebugEnabled()) {
+                    logger.debug(openXlS.toString());
+                }
+                Platform.runLater(new Runnable() {
+
+                    @Override public void run() {
+                        try {
+                            final byte[] bytes = convertTable2Xls();
+                            final File temp = File.createTempFile("xls-temp-" + System.nanoTime(), ".xls");
+                            final FileOutputStream fos = new FileOutputStream(temp);
+                            fos.write(bytes);
+                            fos.flush();
+                            fos.close();
+                            Desktop desktop = Desktop.getDesktop();
+                            desktop.open(temp);
+                        } catch (IOException e1) {
+                            logger.error(e1);
+                        }
+                    }
+                });
+            }
+        });
+
+
+        final MenuItem saveXlS = new MenuItem("contextmenu.savexls");
+        saveXlS.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                Platform.runLater(new Runnable() {
+                    @Override public void run() {
+                        try {
+                            final byte[] bytes = convertTable2Xls();
+                            FileChooser fileChooser = new FileChooser();
+                            fileChooser.setTitle("contextmenu.savexls");
+                            fileChooser.setInitialDirectory(
+                                    new File(System.getProperty("contextmenu.defaultdir"))
+                            );
+                            final File targetFile = fileChooser.showSaveDialog(filteredTableView.getScene().getWindow());
+                            if (targetFile != null) {
+                                final FileOutputStream fos = new FileOutputStream(targetFile);
+                                fos.write(bytes);
+                                fos.flush();
+                                fos.close();
+                            } else {
+                                if (logger.isDebugEnabled()) {
+                                    logger.debug("keine Ziel ausgewählt..");
+                                }
+                            }
+                        } catch (IOException e1) {
+                            logger.error(e1);
+                        }
+                    }
+                });
+            }
+        });
+        MenuItem copyTableCSV2Clipboard = new MenuItem("contextmenu.copyclipboard");
+        copyTableCSV2Clipboard.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                Platform.runLater(new Runnable() {
+                    @Override public void run() {
+                        final StringBuilder clipboardString = convertTable2CSV();
+                        final ClipboardContent content = new ClipboardContent();
+                        content.putString(clipboardString.toString());
+                        Clipboard.getSystemClipboard().setContent(content);
+                    }
+                });
+            }
+        });
+
+        //send csv as mail
+        //send xls as mail
+        //save as pdf
+        //zoom view
+
+        MenuItem copyTableImage2Clipboard = new MenuItem("contextmenu.copyimage");
+        copyTableImage2Clipboard.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                Platform.runLater(new Runnable() {
+                    @Override public void run() {
+                        final WritableImage snapImage = filteredTableView.snapshot(new SnapshotParameters(), null);
+                        final ClipboardContent content = new ClipboardContent();
+                        content.putImage(snapImage);
+                        Clipboard.getSystemClipboard().setContent(content);
+                    }
+                });
+            }
+        });
+
+
+        getItems().addAll(openCSV, saveCSV, openXlS, saveXlS, copyTableCSV2Clipboard,copyTableImage2Clipboard);
+    }
+
+
+    private StringBuilder convertTable2CSV() {
+        final StringBuilder clipboardString = new StringBuilder();
+//        final ObservableList<TableColumn<TransientRechnung, ?>> columns = filteredTableView.getColumns();
+//        for (final TableColumn<TransientRechnung, ?> column : columns) {
+//            clipboardString.append(column.getText() + ";");
+//        }
+//        clipboardString.append('\n');
+//        final ObservableList<TransientRechnung> items = filteredTableView.getItems();
+//
+//        for (final TransientRechnung item : items) {
+//            final String rechnungseingang = item.getRechnungseingang();
+//            clipboardString.append(rechnungseingang + ";");
+//            final String kreditor = item.getKreditor();
+//            clipboardString.append(kreditor + ";");
+//            final String rechnungsnummer = item.getRechnungsnummer();
+//            clipboardString.append(rechnungsnummer + ";");
+//            final double betrag = item.getBetrag();
+//            final String betragEUR = NumberFormat.getCurrencyInstance(Locale.GERMANY).format(betrag);
+//            clipboardString.append(betragEUR + ";");
+//
+//            final String abteilung = item.getAbteilung();
+//            clipboardString.append(abteilung + ";");
+//            final String vermerk = item.getVermerk();
+//            clipboardString.append(vermerk + ";");
+//            final String ersteller = item.getErsteller();
+//            clipboardString.append(ersteller + ";");
+//            final String erstellungsDatum = item.getErstellungsDatum();
+//            clipboardString.append(erstellungsDatum + ";");
+//            final String bearbeiter = item.getBearbeiter();
+//            clipboardString.append(bearbeiter + ";");
+//            final String bearbeitungsDatum = item.getBearbeitungsDatum();
+//            clipboardString.append(bearbeitungsDatum + ";");
+//            final String status = item.getStatus();
+//            clipboardString.append(status + ";");
+//            final String zahlungsstatus = item.getZahlungsstatus();
+//            clipboardString.append(zahlungsstatus + ";");
+//            clipboardString.append('\n');
+//        }
+        return clipboardString;
+    }
+
+    private byte[] convertTable2Xls() throws IOException {
+//        final ObservableList<TableColumn<TransientRechnung, ?>> columns = filteredTableView.getColumns();
+//        final HSSFWorkbook wb = new HSSFWorkbook();
+//        final HSSFCellStyle csHeader = ExcelUtil.createCellStyle(wb, HSSFFont.BOLDWEIGHT_BOLD, HSSFColor.WHITE.index, null, false);
+//        csHeader.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+//        csHeader.setFillBackgroundColor(HSSFColor.GREY_50_PERCENT.index);
+//        csHeader.setFillForegroundColor(HSSFColor.GREY_50_PERCENT.index);
+//        csHeader.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+//
+//        final HSSFSheet sheet = ExcelUtil.getOrCreateSheet(wb, "Eingansgbuch");
+//        int row = 0;
+//        short colNr = 0;
+//        for (final TableColumn<TransientRechnung, ?> column : columns) {
+//            final String columnText = column.getText();
+//            ExcelUtil.setCellValue(sheet, row, colNr, columnText, csHeader);
+//            colNr++;
+//        }
+//        row++;
+//
+//        final HSSFCellStyle csData = ExcelUtil.createCellStyle(wb, HSSFFont.BOLDWEIGHT_NORMAL, HSSFColor.BLACK.index, null, false);
+//
+//        //data
+//        final ObservableList<TransientRechnung> items = filteredTableView.getItems();
+//        for (final TransientRechnung item : items) {
+//            final String rechnungseingang = item.getRechnungseingang();
+//            ExcelUtil.setCellValue(sheet, row, (short) 0, rechnungseingang, csData);
+//            final String kreditor = item.getKreditor();
+//            ExcelUtil.setCellValue(sheet, row, (short) 1, kreditor, csData);
+//            final String rechnungsnummer = item.getRechnungsnummer();
+//            ExcelUtil.setCellValue(sheet, row, (short) 2, rechnungsnummer, csData);
+//            final double betrag = item.getBetrag();
+////                        final String betragEUR = NumberFormat.getCurrencyInstance(Locale.GERMANY).format(betrag);
+//
+//            HSSFDataFormat cf = wb.createDataFormat();
+//            HSSFCellStyle currencyCellStyle = wb.createCellStyle();
+//            currencyCellStyle.setDataFormat(cf.getFormat("#,##0.00\\ €"));
+//            ExcelUtil.setCellValue(sheet, row, (short) 3, betrag, currencyCellStyle);
+//
+//            final String abteilung = item.getAbteilung();
+//            ExcelUtil.setCellValue(sheet, row, (short) 4, abteilung, csData);
+//            final String vermerk = item.getVermerk();
+//            ExcelUtil.setCellValue(sheet, row, (short) 5, vermerk, csData);
+//            final String ersteller = item.getErsteller();
+//            ExcelUtil.setCellValue(sheet, row, (short) 6, ersteller, csData);
+//            final String erstellungsDatum = item.getErstellungsDatum();
+//            ExcelUtil.setCellValue(sheet, row, (short) 7, erstellungsDatum, csData);
+//            final String bearbeiter = item.getBearbeiter();
+//            ExcelUtil.setCellValue(sheet, row, (short) 8, bearbeiter, csData);
+//            final String bearbeitungsDatum = item.getBearbeitungsDatum();
+//            ExcelUtil.setCellValue(sheet, row, (short) 9, bearbeitungsDatum, csData);
+//            final String status = item.getStatus();
+//            ExcelUtil.setCellValue(sheet, row, (short) 10, status, csData);
+//            final String zahlungsstatus = item.getZahlungsstatus();
+//            ExcelUtil.setCellValue(sheet, row, (short) 11, zahlungsstatus, csData);
+//            row++;
+//        }
+//        return ExcelUtil.wbToByteArray(wb);
+        return null;
+    }
+
+
+}
