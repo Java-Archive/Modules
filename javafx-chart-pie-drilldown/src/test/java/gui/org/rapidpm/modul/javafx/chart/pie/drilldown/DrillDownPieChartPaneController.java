8621b96701269c026901f840f90ddd5b7d4b10e3
Sven Ruppert <sven.ruppert@siteos.de>
2013-09-02 18:46:18 +0200

    MOD-62 - Erzeugen von Aggregatoren, DrillDownPieChart

---
+++ javafx-chart-pie-drilldown/src/test/java/gui/org/rapidpm/modul/javafx/chart/pie/drilldown/DrillDownPieChartPaneController.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright [2013] [www.rapidpm.org / Sven Ruppert (sven.ruppert@rapidpm.org)]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package gui.org.rapidpm.modul.javafx.chart.pie.drilldown;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+
+import gui.org.rapidpm.modul.javafx.chart.pie.drilldown.model.TransientDemoDataRow;
+import javafx.fxml.FXML;
+import org.rapidpm.demo.cdi.commons.fx.CDIJavaFxBaseController;
+import org.rapidpm.modul.javafx.chart.pie.drilldown.DrillDownPieChartMapAggregator;
+
+/**
+ * User: Sven Ruppert
+ * Date: 02.09.13
+ * Time: 16:49
+ */
+public class DrillDownPieChartPaneController implements CDIJavaFxBaseController {
+
+    @FXML DemoDrillDownPieChart piechart;
+
+
+    @Inject Instance<VornameAggregator> vornameAggregatorInstance;
+    @Inject Instance<NachnameAggregator> nachnameAggregatorInstance;
+    @Inject Instance<DatumAggregator> datumAggregatorInstance;
+    @Inject Instance<BetragAggregator> betragAggregatorInstance;
+
+
+    @Override public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //setze Aggregatoren
+        final VornameAggregator vornameAggregator = vornameAggregatorInstance.get();
+
+        final NachnameAggregator nachnameAggregator = nachnameAggregatorInstance.get();
+        vornameAggregator.setNextLevelAggregator(nachnameAggregator);
+
+        final DatumAggregator datumAggregator = datumAggregatorInstance.get();
+        nachnameAggregator.setNextLevelAggregator(datumAggregator);
+
+        final BetragAggregator betragAggregator = betragAggregatorInstance.get();
+        datumAggregator.setNextLevelAggregator(betragAggregator);
+
+        piechart.setRootAggregator(vornameAggregator);
+
+    }
+
+
+    public void init() {
+        piechart.init();
+    }
+
+
+    public static class BetragAggregator extends DrillDownPieChartMapAggregator<TransientDemoDataRow> {
+
+
+        /**
+         * Calculate the Value for the PiChart.Data based on the aggregated values
+         *
+         * @param aggregatedValues
+         * @return
+         */
+        @Override public double aggregateValue(List<TransientDemoDataRow> aggregatedValues) {
+            double betrag = 0;
+            for (final TransientDemoDataRow aggregatedValue : aggregatedValues) {
+                betrag = betrag + aggregatedValue.getBetrag();
+            }
+
+            return betrag;
+        }
+
+        @Override public String getLevelName() {
+            return "Betrag";
+        }
+
+        /**
+         * Key sollte n-dimensional sein..
+         *
+         * @param transientDemoDataRow
+         * @return
+         */
+        @Override public String getKeyElement(TransientDemoDataRow transientDemoDataRow) {
+            return transientDemoDataRow.getBetrag() + " € Beträge";
+        }
+    }
+
+    public static class DatumAggregator extends DrillDownPieChartMapAggregator<TransientDemoDataRow> {
+
+
+        /**
+         * Calculate the Value for the PiChart.Data based on the aggregated values
+         *
+         * @param aggregatedValues
+         * @return
+         */
+        @Override public double aggregateValue(List<TransientDemoDataRow> aggregatedValues) {
+            return aggregatedValues.size();
+        }
+
+        @Override public String getLevelName() {
+            return "Datum";
+        }
+
+        /**
+         * Key sollte n-dimensional sein..
+         *
+         * @param transientDemoDataRow
+         * @return
+         */
+        @Override public String getKeyElement(TransientDemoDataRow transientDemoDataRow) {
+            return transientDemoDataRow.getDatum();
+        }
+    }
+
+    public static class VornameAggregator extends DrillDownPieChartMapAggregator<TransientDemoDataRow> {
+
+
+        /**
+         * Calculate the Value for the PiChart.Data based on the aggregated values
+         *
+         * @param aggregatedValues
+         * @return
+         */
+        @Override public double aggregateValue(List<TransientDemoDataRow> aggregatedValues) {
+            return aggregatedValues.size();
+        }
+
+        @Override public String getLevelName() {
+            return "Vorname";
+        }
+
+        /**
+         * Key sollte n-dimensional sein..
+         *
+         * @param transientDemoDataRow
+         * @return
+         */
+        @Override public String getKeyElement(TransientDemoDataRow transientDemoDataRow) {
+            return transientDemoDataRow.getVorname();
+        }
+    }
+
+
+    public static class NachnameAggregator extends DrillDownPieChartMapAggregator<TransientDemoDataRow> {
+
+
+        /**
+         * Calculate the Value for the PiChart.Data based on the aggregated values
+         *
+         * @param aggregatedValues
+         * @return
+         */
+        @Override public double aggregateValue(List<TransientDemoDataRow> aggregatedValues) {
+            return aggregatedValues.size();
+        }
+
+        @Override public String getLevelName() {
+            return "Nachname";
+        }
+
+        /**
+         * Key sollte n-dimensional sein..
+         *
+         * @param transientDemoDataRow
+         * @return
+         */
+        @Override public String getKeyElement(TransientDemoDataRow transientDemoDataRow) {
+            return transientDemoDataRow.getNachname();
+        }
+    }
+
+
+    public DemoDrillDownPieChart getPiechart() {
+        return piechart;
+    }
+
+    public void setPiechart(DemoDrillDownPieChart piechart) {
+        this.piechart = piechart;
+    }
+}
